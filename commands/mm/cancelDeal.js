45accdbedff12655d352f500a010100aee5bd501
Bewkuf-Insaan <bewkufinsaan1@gmail.com>
2026-02-08 02:09:00 +0530

    Update cancelDeal.js

--- commands/mm/cancelDeal.js
+++ commands/mm/cancelDeal.js
@@ -109,5 +109,14 @@ module.exports = {
       interaction.guild.roles.everyone.id,
       { ViewChannel: false }
     );
+    setTimeout(async () => {
+  try {
+    await interaction.channel.delete();
+  } catch (err) {
+    console.error("Ticket delete error:", err);
+  }
+}, 3000); // delete after 3 seconds
+
   }
 };
+
